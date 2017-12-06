8cabc03d42b9756f7460dbf28e6448dfc046a887
Kamila Wasilewska <kamwas93@gmail.com>
2017-12-06 14:39:02 +0100

    changed interface of the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
 
     $('.checkByGeo').on('click', function() {
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 var longitude = position.coords.longitude;
                 var latitude = position.coords.latitude;
 
                 var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lon=' + longitude + '&lat=' + latitude + '&units=metric&lang=pl&appid=1caabd42d7e148e50786965db859995f';
-                console.log(apiUrl);
 
                 searchText = 'select * from weather.forecast where woeid in (select woeid from geo.places where text="(' + latitude + ',' + longitude + ')") and u="c"';
                 var apiUrl2 = 'https://query.yahooapis.com/v1/public/yql?q=' + searchText + '&format=json';
-                console.log(apiUrl2);
 
                 getWeather('openweathermap', apiUrl);
                 getWeather('yahoo', apiUrl2);
@@ -51,11 +50,9 @@ function getWeather(provider, apiUrl) {
             $.getJSON(apiUrl, function(data) {
                 var city = data.name;
                 var country = data.sys.country;
-                var iconCode = data.weather[0].icon;
+                var iconCode = data.weather[0].id;
                 var description = data.weather[0].description;
                 var tempC = data.main.temp;
-                var tempMinC = data.main.temp_min;
-                var tempMaxC = data.main.temp_max;
                 var pressure = data.main.pressure;
                 var humidity = data.main.humidity;
                 var wind = data.wind.speed;
@@ -66,14 +63,26 @@ function getWeather(provider, apiUrl) {
                     tempC = data.main.temp.toFixed(1);
                 }
 
-                $("#iconCode").html("<img src='http://openweathermap.org/img/w/" + iconCode + ".png' alt='Weather icon'>" + " <br />" + description);
+                document.getElementById('currentWeatherCard').style.visibility = 'visible';
+                document.getElementById('forecastCard').style.visibility = 'visible';
+
+                $("#iconCode").html("<img class='wi wi-owm-" + iconCode + "' alt=' '>");
+                $("#description").html(description);
                 $("#cityName").html(city + ", " + country);
                 $("#temperature").html(tempC + "°C");
-                $("#details").html("Ciśnienie: " + pressure + " hPa<br /> Wilgotność: " + humidity + " %<br />Min temperatura: " + tempMinC + " &#8451;<br />Max temperatura: " + tempMaxC + " &#8451; <br />Wiatr: " + convertedWind + " km/h");
+                $("#pressure").html(pressure + " hPa");
+                $("#humidity").html(humidity + " %");
+                $("#wind").html(convertedWind + " km/h");
             });
             break;
         case 'yahoo':
             $.getJSON(apiUrl, function(data) {
+                var tempMinCurrentC = data.query.results.channel.item.forecast[0].low;
+                var tempMaxCurrentC = data.query.results.channel.item.forecast[0].high;
+
+                $("#tempMinC").html(tempMinCurrentC + " &#8451;");
+                $("#tempMaxC").html(tempMaxCurrentC + " &#8451;");
+
                 for (var i = 1; i <= 3; i++) {
                     var date = data.query.results.channel.item.forecast[i].date;
                     var day = data.query.results.channel.item.forecast[i].day;
@@ -85,15 +94,18 @@ function getWeather(provider, apiUrl) {
                     switch (i) {
                         case 1:
                             $('#day1').html(day + ", " + date);
-                            $('.tomorrow').html("<img src='http://l.yimg.com/a/i/us/we/52/" + code + ".gif' alt='Weather icon'>" + " <br />" + text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
+                            $('#weatherIcon1').html("<img class='wi wi-yahoo-" + code + "' alt=' '>");
+                            $('.tomorrow').html(text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
                             break;
                         case 2:
                             $('#day2').html(day + ", " + date);
-                            $('.overmorrow').html("<img src='http://l.yimg.com/a/i/us/we/52/" + code + ".gif' alt='Weather icon'>" + " <br />" + text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
+                            $('#weatherIcon2').html("<img class='wi wi-yahoo-" + code + "' alt=' '>");
+                            $('.overmorrow').html(text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
                             break;
                         case 3:
                             $('#day3').html(day + ", " + date);
-                            $('.dayAfterOvermorrow').html("<img src='http://l.yimg.com/a/i/us/we/52/" + code + ".gif' alt='Weather icon'>" + " <br />" + text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
+                            $('#weatherIcon3').html("<img class='wi wi-yahoo-" + code + "' alt=' '>");
+                            $('.dayAfterOvermorrow').html(text + "<br />Min: " + tempMinC + "°C, Max: " + tempMaxC + "°C");
                             break;
                     }
                 }
